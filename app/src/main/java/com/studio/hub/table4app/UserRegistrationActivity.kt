b635fffff46cab64a000b71cfda94dc3169e8d78
luisLopez <louislopez.dev@gmail.com>
2018-11-02 01:28:06 -0500

    Maquetacion pantalla Registro.

---
+++ app/src/main/java/com/studio/hub/table4app/UserRegistrationActivity.kt
@@ -0,0 +1,12 @@
+package com.studio.hub.table4app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class UserRegistrationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_registration)
+    }
+}
